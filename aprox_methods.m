6dbede455c864a1b6db5c7f1c4887115b9fe8833
clenilson willames <willamesclenilson@gmail.com>
2025-02-09 18:51:44 -0300

    Update aprox_methods.m

--- aprox_methods.m
+++ aprox_methods.m
@@ -1,85 +1,79 @@
+% For Galerkin Method (l = 1)
+aprox_methods(1, 1)
+
+% For Least Squares Method (l = 2)
+aprox_methods(1, 2)
+
+% For Collocation Method (l = 3)
+aprox_methods(1, 3)
+
+
 function u_at_x_05 = aprox_methods(N, l)
-    % Função que implementa o método de Galerkin e outros métodos de resíduos ponderados
-    %
-    % Entradas:
-    % N  - Número de termos da série
-    % l  - Tipo de método de peso (1 = Galerkin, 2 = Least Squares, 3 = Método de Resíduo Ponderado)
+    % Function that implements Galerkin, Least Squares, and Collocation methods
+    % N: number of terms in the series
+    % l: type of method (1 = Galerkin, 2 = Least Squares, 3 = Collocation)
+    
+    % Defining symbolic variables
+    syms x a;
 
-    % Definições simbólicas
-    syms x;
-    a = sym('a', [1 N]); % Vetor de coeficientes simbólicos
+    % Initializing the approximator function u(x)
+    u = 0; 
 
-    % Inicializando a função aproximadora
-    u = 0;
-
-    % Loop para adicionar os termos da série
+    % Loop to add terms in the series (summing up to the N-th term)
     for k = 1:N
-        u = u + a(k) * x^k * (1 - x); % Termo a_k * x^k * (1 - x)
+        u = u + a^k * x^k * (1 - x); % Adds the term a_k * x^k * (1 - x)
     end
 
-    % Definindo a função onde R é o resíduo
-    f = diff(u, x, 2) - u + x; % Derivada segunda de u menos u mais x
+    % Defining the equation representing the residue f
+    f = diff(u, x, 2) - u + x; % Second derivative of u minus u plus x
 
-    % Escolha do método de peso (w)
-    w = escolher_metodo_w(u, f, a, l, N);
-
-    % Resolvendo o sistema de equações
-    eqs = int(f .* w, x, 0, 1) == 0;
-    sol = solve(eqs, a);
-
-    % Substituindo os valores de 'a_n' na função aproximadora
-    for k = 1:N
-        u = subs(u, a(k), sol(k)); % Substituindo a_k com o valor calculado de sol(k)
-    end
-
-    % Avaliando a função aproximadora em x = 0.5
-    u_at_x_05 = double(subs(u, x, 0.5));
-
-    % Exibindo os resultados
-    disp('Valores de a_n (solução para a):');
-    disp(sol);
-    disp('Valor de u em x = 0.5:');
-    disp(u_at_x_05);
-end
-
-function w = escolher_metodo_w(u, f, a, l, N)
-    % Função para escolher o método de peso (w) baseado no valor de 'l'
-    %
-    % Entradas:
-    % u  - Função aproximadora
-    % f  - Resíduo (função)
-    % a  - Parâmetro a
-    % l  - Identificador do método de peso (1 = Galerkin, 2 = Least Squares, 3 = Resíduo Ponderado)
-    % N  - Número de termos na série
-
-    syms x;
-    w = sym(zeros(1, N)); % Inicializando a função de peso
-
+    % Weight function w, which depends on the chosen method
     switch l
-        case 1  % Método de Galerkin
-            for k = 1:N
-                w(k) = diff(u, a(k)); % Derivada de u em relação a a_k
-            end
-
-        case 2  % Método dos Mínimos Quadrados
-            for k = 1:N
-                w(k) = diff(f, a(k)); % Derivada do resíduo em relação a a_k
-            end
-
-        case 3  % Método de Resíduo Ponderado
-            for k = 1:N
-                w(k) = x^k; % Função de peso genérica baseada em x^k
-            end
-
-        otherwise
-            error('Método de peso desconhecido! Escolha entre 1 (Galerkin), 2 (Least Squares) ou 3 (Weighted Residual).');
+        case 1  % Galerkin Method
+            w = diff(u, a);  % Derivative of u with respect to a
+        case 2  % Least Squares Method
+            w = diff(f, a);  % Derivative of f with respect to a
+        case 3  % Collocation Method
+            % Defining the collocation point, for example x = 0.5
+            x_i = 0.5;
+            
+            % Evaluating the residue at x_i
+            residuo = subs(f, x, x_i);  % Substituting x_i in the residue equation
+            
+            % The residue must be forced to zero at x_i
+            Aprox = residuo;  % Residue at x_i
     end
+
+    % For Galerkin and Least Squares methods, we use the integral.
+    if l ~= 3
+        % Multiplying the residue by the weight function
+        Aprox = f * w;
+
+        % Defining the function interval from 0 < x < 1
+        inter1 = 0;
+        inter2 = 1;
+
+        % Defining the values of the approximator function (integrating the residue times the weight function)
+        L = int(Aprox, x, inter1, inter2);
+
+        % Displaying the integral value (solving for L = 0)
+        a_n = solve(L == 0, a);  % Solving the integral to find the solution for a
+    else
+        % In the Collocation method, integration is not needed
+        a_n = solve(Aprox == 0, a);  % Solving for a_n
+    end
+
+    % Displaying the value of a_n (solution for a)
+    disp('Value of a_n (solution for a):');
+    disp(a_n);  
+
+    % Substituting the value of a_n into the function u(x)
+    u_sub = subs(u, a, a_n);  % Substituting a_n into u
+
+    % Evaluating the approximator function at x = 0.5
+    u_at_x_05 = double(subs(u_sub, x, 0.5));  % Evaluating the function at x = 0.5
+
+    % Displaying the value of u at x = 0.5
+    disp('Value of u at x = 0.5:');
+    disp(u_at_x_05);  
 end
-
-% Exemplo de uso:
-N = 3; % Número de termos da série
-l = 1; % Método de Galerkin (1 = Galerkin, 2 = Least Squares, 3 = Weighted Residual)
-
-resultado = aprox_methods(N, l);
-disp(['Resultado em x = 0.5: ', num2str(resultado)]);
-
